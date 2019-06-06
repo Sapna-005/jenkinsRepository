dc1058dd1ed67dde00996fd96fdfd1624f331536
Sapna <sapnachourasiya005@gmail.com>
2019-06-06 17:44:10 +0530

    build1

---
+++ src/test/java/mavendemo/Demo.java
@@ -0,0 +1,22 @@
+package mavendemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+@Test
+public static void openBrowser()
+{
+	System.setProperty("webdriver.chrome.driver","E:\\selenium\\Selenium_Project\\driver\\chromedriver.exe");
+	WebDriver driver= new ChromeDriver();
+	driver.get("https://www.seleniumhq.org/");
+	System.out.println(driver.getTitle());
+	driver.findElement(By.xpath("//a[@title='Get Selenium']"));
+	System.out.println(driver.getTitle());
+	
+	driver.close();
+}
+}
